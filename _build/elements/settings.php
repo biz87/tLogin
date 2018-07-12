15f2723ad00c64dcb134145a2a2d2e216eb46017
Nikolay Savin <info@megawebs.kz>
2018-07-12 12:27:40 +0600

    fix settings

--- _build/elements/settings.php
+++ _build/elements/settings.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    'tlogin_register' => [
+    'register' => [
         'xtype' => 'combo-boolean',
         'value' => true,
         'area' => 'tlogin_main',
     ],
-    'tLogin_bot_token' => [
+    'bot_token' => [
         'xtype' => 'textfield',
         'value' => '',
         'area' => 'tlogin_main'
     ],
-    'tLogin_bot_name' => [
+    'bot_name' => [
         'xtype' => 'textfield',
         'value' => '',
         'area' => 'tlogin_main'
--- core/components/tlogin/model/tlogin/tlogin.class.php
+++ core/components/tlogin/model/tlogin/tlogin.class.php
@@ -56,11 +56,7 @@ class tLogin
             return $auth_data;
         }
 
-        //   if (isset($_COOKIE['tg_user'])) {
-        //     $auth_data_json = urldecode($_COOKIE['tg_user']);
-        //     $auth_data = json_decode($auth_data_json, true);
-        //     return $auth_data;
-        //   }
+
         return false;
     }
 
